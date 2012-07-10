17d8fe3a2be54f84b57b097d82682baf78894fc4
Koji Iwasaki <iwasaki@cresc.com>
2012-07-10 16:02:06 +0900

    メインの処理を追加

--- main.php
+++ main.php
@@ -13,3 +13,13 @@ use Madapaja\Ray\Di\Sample01\Module\UserModule;
 
 $di = new Injector(new Container(new Forge(new Config(new Annotation(new Definition)))));
 $di->setModule(new UserModule);
+
+$user = $di->getInstance('Madapaja\Ray\Di\Sample01\Model\User');
+
+$user->init();
+$user->createUser('Koriym', rand(18,35));
+$user->createUser('Bear', rand(18,35));
+$user->createUser('Yoshi', rand(18,35));
+
+$users = $user->readUsers();
+var_export($users);
