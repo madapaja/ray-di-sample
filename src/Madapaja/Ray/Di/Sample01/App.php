4a3692e60f84a8a7996e3c97d62e98a66e465746
Koji Iwasaki <iwasaki@cresc.com>
2012-07-10 23:04:36 +0900

    Injecter の生成方法を変更

--- src/Madapaja/Ray/Di/Sample01/App.php
+++ src/Madapaja/Ray/Di/Sample01/App.php
@@ -3,11 +3,6 @@
 namespace Madapaja\Ray\Di\Sample01;
 
 use Ray\Di\Injector;
-use Ray\Di\Container;
-use Ray\Di\Forge;
-use Ray\Di\Config;
-use Ray\Di\Annotation;
-use Ray\Di\Definition;
 
 use Madapaja\Ray\Di\Sample01\Module\UserModule;
 
@@ -15,9 +10,7 @@ class App
 {
     public function run()
     {
-        $di = new Injector(new Container(new Forge(new Config(new Annotation(new Definition)))));
-        $di->setModule(new UserModule);
-
+        $di = Injector::create([new UserModule]);
         $user = $di->getInstance('Madapaja\Ray\Di\Sample01\Model\User');
 
         $user->createUser('Koriym', mt_rand(18,35));
